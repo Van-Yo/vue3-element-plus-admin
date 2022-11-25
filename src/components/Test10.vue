7ba1f111dff7c776e9dcb3410bbfe83237f37852
wanrong <jokerwanr@gmail.com>
2022-11-25 15:47:53 +0800

    defineProps,defineEmits

---
+++ src/components/Test10.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        父组件的值：{{doc}}
+        <!-- 通过自定义属性value传递值doc -->
+        <!-- 通过子组件自定义属性func传递值、方法getChildInfo -->
+        <Test10props :value="doc" @func="getChildInfo"/>
+    </div>
+    
+</template>
+<script setup>
+// *************************组件传值：父组件***************************
+import Test10props from './Test10props.vue'
+import {ref} from 'vue'
+
+const doc = ref('hello')
+
+const getChildInfo = (val)=>{
+    console.log(val);
+    doc.value += val
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/Test10props.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <!-- 使用父组件传递过来的值 -->
+        父组件的值:{{props.value}}
+    </div>
+    <button @click="handClick">子传值给父</button>
+</template>
+<script setup>
+// *************************组件传值：子组件***************************
+// 从 vue 中引入defineProps, defineEmits
+import {defineProps,defineEmits} from 'vue'
+
+// 接收父组件传递过来的值
+const props = defineProps(['value'])
+// 接收父组件传递过来的方法
+const emit = defineEmits(['func'])
+
+const handClick = ()=>{
+    //调用父组件传递过来的方法，传入参数
+    emit('func','world')
+}
+
+</script>
+<style lang="">
+    
+</style>
