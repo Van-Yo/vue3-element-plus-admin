e43ee4207ac7426f31d68f83695d8902b3df64d7
wanrong <jokerwanr@gmail.com>
2022-12-02 13:59:09 +0800

    router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import VueCesium from 'vue-cesium'
 import App from './App.vue'
 import createStore from './store/index'
+import router from './router/index.js'
 
 import 'vue-cesium/dist/index.css'
 
@@ -10,5 +11,6 @@ import 'vue-cesium/dist/index.css'
 const app = createApp(App)
 app.use(VueCesium)
 app.use(createStore)
+app.use(router)
 
 app.mount('#app')
