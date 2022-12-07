4feda5e2cf49ba07dc29281f32a6d6d3c5957301
wanrong <jokerwanr@gmail.com>
2022-12-07 15:14:47 +0800

    登录注册切换

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -3,7 +3,9 @@
     <div class="login-box">
       <el-input v-model="user" placeholder="用户名" />
       <el-input v-model="password" placeholder="密码" />
-      <el-button type="primary">登录</el-button>
+      <p @click="changeMode">{{ mode?'注册':'登录' }}</p>
+      <el-button v-if="mode" type="primary" @click="login">登录</el-button>
+      <el-button v-else type="primary" @click="register">注册</el-button>
     </div>
   </div>
 </template>
@@ -11,6 +13,16 @@
 import { ref } from 'vue'
 const user = ref('')
 const password = ref('')
+const mode = ref(true)
+const changeMode = () => {
+  mode.value = !mode.value
+}
+const login = () => {
+  console.log(user.value, password.value)
+}
+const register = () => {
+  console.log(user.value, password.value)
+}
 </script>
 <style lang="scss">
 .login-area{
@@ -26,6 +38,9 @@ const password = ref('')
         width:500px;
         height:400px;
         border:1px solid red;
+        p{
+            cursor: pointer;
+        }
     }
 }
 
