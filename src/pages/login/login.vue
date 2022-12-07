144c16147927a0e7e3cc23200c5966057f2de18e
wanrong <jokerwanr@gmail.com>
2022-12-07 11:39:12 +0800

    登陆界面init

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,13 +1,31 @@
 <template lang="">
-    <div>
-        <el-button type="primary">login</el-button>
-        <el-input v-model="input1" placeholder="Please input" />
+    <div class="login-area">
+        <div class="login-box">
+            <el-input v-model="user" placeholder="用户名" />
+            <el-input v-model="password" placeholder="密码" />
+            <el-button type="primary">登录</el-button>
+        </div>
     </div>
 </template>
 <script setup>
 import { ref } from 'vue'
-const input1 = ref('')
+const user = ref('')
+const password = ref('')
 </script>
-<style lang="">
-    
+<style>
+.login-area{
+    width:100vw;
+    height:100vh;
+    overflow:hidden;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    background:url('../../static/banner-default.jpg');
+    background-size:100% 100%;
+}
+    .login-box{
+        width:500px;
+        height:400px;
+        border:1px solid red;
+    }
 </style>
