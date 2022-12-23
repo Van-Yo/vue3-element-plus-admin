cd40d217257cd783d03bc6790d2cfd577a8c049d
wanrong <jokerwanr@gmail.com>
2022-12-23 15:24:13 +0800

    setup语法糖-ref, reactive, computed

---
+++ src/components/TestSetupApi1.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ name }}</h1>
+  <h1>{{ info.age }}</h1>
+  <h1>{{ brief }}</h1>
+</template>
+
+<script setup>
+/**
+ * ref, reactive, computed
+*/
+import { ref, reactive, computed } from 'vue'
+const name = ref('wrrrrrr')
+const info = reactive({
+  age: 18,
+  hobby: 'basketball'
+})
+const brief = computed(() => {
+  return `姓名：${name.value}，年龄：${info.age}`
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
