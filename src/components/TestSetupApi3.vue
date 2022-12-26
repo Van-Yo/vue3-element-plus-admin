623d7410a3aa7515e1e51b4c83712d58a6aa972d
wanrong <jokerwanr@gmail.com>
2022-12-26 14:04:08 +0800

    useSlots

---
+++ src/components/TestSetupApi3.vue
@@ -0,0 +1,26 @@
+<template>
+  <TestSetupApi3Slots>
+    <template #title>
+      标题1
+    </template>
+    <template #content>
+      内容1
+    </template>
+  </TestSetupApi3Slots>
+  <TestSetupApi3Slots>
+    <template #title>
+      标题2
+    </template>
+    <template #content>
+      内容2
+    </template>
+  </TestSetupApi3Slots>
+</template>
+
+<script setup>
+import TestSetupApi3Slots from './TestSetupApi3Slots.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TestSetupApi3Slots.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="title">
+    <slot name="title" />
+  </div>
+  <div class="content">
+    <slot name="content" />
+  </div>
+</template>
+
+<script setup>
+import { useSlots, onMounted } from 'vue'
+const slots = useSlots()
+onMounted(() => {
+  console.log(slots.title())
+})
+</script>
+
+<style lang="scss" scoped>
+.title{
+  width:700px;
+  height:40px;
+  background:red;
+}
+.content{
+  width:700px;
+  height:600px;
+  border:1px solid green;
+}
+</style>
